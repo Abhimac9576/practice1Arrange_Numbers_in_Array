1da285c6edbe796735f30375d18c8ac5bead0ec1
Abhishek Kumar <abhimac9576@gmail.com>
2022-01-10 21:31:40 +0530

    second page

---
+++ practice1/arrange.cpp
@@ -0,0 +1,8 @@
+//
+//  arrange.cpp
+//  practice1
+//
+//  Created by ABHI MAC on 10/01/22.
+//
+
+#include "arrange.hpp"
---
+++ practice1/arrange.hpp
@@ -0,0 +1,13 @@
+//
+//  arrange.hpp
+//  practice1
+//
+//  Created by ABHI MAC on 10/01/22.
+//
+
+#ifndef arrange_hpp
+#define arrange_hpp
+
+#include <stdio.h>
+
+#endif /* arrange_hpp */
