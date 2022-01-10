2abd73225c226b7c5889b6253494ec8b6e59220c
Abhishek Kumar <abhimac9576@gmail.com>
2022-01-10 21:28:05 +0530

    Arrange Number in Array

--- practice1/main.cpp
+++ practice1/main.cpp
@@ -6,9 +6,58 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//#include "solution.h"
+
+void arrange(int *arr, int n)
+{
+
+    int j = n;
+        
+    
+    for (int i = 0;i<=(n-1)/2;i++)
+    {
+        arr[i]= 2 *i +1;
+        
+    }
+    
+    for (int i = (n+1)/2 ;i<n;i++)
+    {
+      if (n % 2!=0){
+          
+      arr[i]= j-1;
+            
+          j = j-2;
+         
+     }
+        
+       else if (n%2 == 0)
+         {
+           arr[i]= j;
+            j = j-2;
+         }
+    }
+}
+         
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int *arr = new int[n];
+        arrange(arr, n);
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        delete [] arr;
+    }
+
+    
 }
